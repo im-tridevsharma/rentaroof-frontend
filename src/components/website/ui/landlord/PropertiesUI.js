1fe89fd7228e08e1759bd3d6424cd5d20490453f
Tridev Sharma <sendmailtotridev@gmail.com>
2021-10-09 19:31:46 +0530

    new changes

--- src/components/website/ui/landlord/PropertiesUI.js
+++ src/components/website/ui/landlord/PropertiesUI.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Card from "../../Card";
 import PropertyGrid from "../../PropertyGrid";
